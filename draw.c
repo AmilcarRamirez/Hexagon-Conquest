45276575869945c0068ed0ef17f7f1b9f7bf661f
AmilcarRamirez <60532724+AmilcarRamirez@users.noreply.github.com>
2020-05-02 18:25:38 -0500

    Update Draw.c
    Now i has color, but problems with the empty spaces

--- draw.c
+++ draw.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "conio.h"
+#include "string.h"
 
 #define HEIGHT 46
 #define WIDTH 91
+#define DEPTH 10
 
 #define ASCII_VER 186
 #define ASCII_HOR 205
@@ -12,13 +14,25 @@
 #define ASCII_SRC 187
 #define ASCII_SLC 201
 
+#define ASCII_BMAYUS 66
+#define ASCII_A 97
+#define ASCII_S 115
+#define ASCII_E 101
+#define ASCII_NUM 35
+
+
+
 typedef struct base_data {
-  int id;
+  char id;
   int x;
   int y;
   int w;
   int h;
 } bd;
+
+typedef struct play_data{
+  int turn;
+} pd;
 /*[0;31m	Red
 [1;31m	Bold Red
 [0;32m	Green
@@ -37,16 +51,16 @@ typedef struct base_data {
 void startUpText();
 void colors();
 
-void createBase(char[HEIGHT][WIDTH], bd);
-void basesGeneration(char[HEIGHT][WIDTH]);
+void createBase(char[HEIGHT][WIDTH][DEPTH], bd);
+void basesGeneration(char[HEIGHT][WIDTH][DEPTH]);
 
-void clearMap(char[HEIGHT][WIDTH]);
-void printMap(char[HEIGHT][WIDTH]);
-//void nameBase(int, int, char, char[HEIGHT][WIDTH]);
+void clearMap(char[HEIGHT][WIDTH][DEPTH]);
+void printMap(char[HEIGHT][WIDTH][DEPTH]);
+void nameBase(bd, char[HEIGHT][WIDTH][DEPTH]);
 
 
 int main(){
-  char map[HEIGHT][WIDTH];
+  char map[HEIGHT][WIDTH][DEPTH];
   startUpText();
   clearMap(map);
   printMap(map);
@@ -60,7 +74,7 @@ void startUpText(){
     getch();
     system("cls");
     printf("\tGame instructions:\n");
-    printf("\n-> Use arrow keys to move the snake.\n\n-> You will be provided foods at the several coordinates of the screen which you have to eat. Everytime you eat a food the length of the snake will be increased by 1 element and thus the score.\n\n-> Here you are provided with three lives. Your life will decrease as you hit the wall or snake's body.\n\n-> YOu can pause the game in its middle by pressing any key. To continue the paused game press any other key once again\n\n-> If you want to exit press esc. \n");
+    printf("\n-> Press a number in your keyboard between 1, 2 and 3.\n\n-> You will be provided foods at the several coordinates of the screen which you have to eat. Everytime you eat a food the length of the snake will be increased by 1 element and thus the score.\n\n-> Here you are provided with three lives. Your life will decrease as you hit the wall or snake's body.\n\n-> YOu can pause the game in its middle by pressing any key. To continue the paused game press any other key once again\n\n-> If you want to exit press esc. \n");
     printf("\n\nPress any key to play game...");
     printf("\033[0m");
     if(getch()==27)
@@ -68,89 +82,131 @@ void startUpText(){
     system("cls");
 }
 
-void basesGeneration(char map[HEIGHT][WIDTH]){
+void basesGeneration(char map[HEIGHT][WIDTH][DEPTH]){
   bd data;
-  data.x = 40;
-  data.y = 15;
+  data.x = 25;
+  data.y = 1;
   data.w = 12;
   data.h = 6;
-  data.id = 1;
+  data.id = '1';
   createBase(map, data);
-  data.x = 53;
-  data.y = 14;
-  data.w = 16;
-  data.h = 8;
-  data.id = 2;
+  data.x = 55;
+  data.y = 1;
+  data.w = 12;
+  data.h = 6;
+  data.id = '2';
   createBase(map, data);
-  data.x = 70;
-  data.y = 13;
-  data.w = 20;
-  data.h = 10;
-  data.id = 3;
+  data.x = 79;
+  data.y = 12;
+  data.w = 12;
+  data.h = 6;
+  data.id = '3';
   createBase(map, data);
-
+  data.x = 79;
+  data.y = 27;
+  data.w = 12;
+  data.h = 6;
+  data.id = '4';
+  createBase(map, data);
+  data.x = 55;
+  data.y = 39;
+  data.w = 12;
+  data.h = 6;
+  data.id = '5';
+  createBase(map, data);
+  data.x = 25;
+  data.y = 39;
+  data.w = 12;
+  data.h = 6;
+  data.id = '6';
+  createBase(map, data);
+  data.x = 1;
+  data.y = 27;
+  data.w = 12;
+  data.h = 6;
+  data.id = '7';
+  createBase(map, data);
+  data.x = 1;
+  data.y = 12;
+  data.w = 12;
+  data.h = 6;
+  data.id = '8';
+  createBase(map, data);
 }
 
-void printMap(char map[HEIGHT][WIDTH]){
+void printMap(char map[HEIGHT][WIDTH][DEPTH]){
   basesGeneration(map);
   for (int i = 0; i < HEIGHT; i++) {
     for (int j = 0; j < WIDTH; j++) {
-      printf("%c", map[i][j]);
+      printf("%s", map[i][j]);
     }
     printf("\n");
   }
 }
 
-void clearMap(char map[HEIGHT][WIDTH]){
+void clearMap(char map[HEIGHT][WIDTH][DEPTH]){
   for (int i = 0; i < HEIGHT; i++) {
     for (int j = 0; j < WIDTH; j++) {
-      map[i][j] = 0;
+      for (int k = 0; k < DEPTH; k++) {
+        map[i][j][k] = 42;
+      }
     }
   }
 }
 
-void createBase(char map[HEIGHT][WIDTH], bd data) {
-  map[data.y][data.x] = ASCII_SLC;
-  map[data.y + data.h ][data.x] = ASCII_ILC; //CHECK SIZE
-  map[data.y][data.x + data.w] = ASCII_SRC;
-  map[data.y + data.h ][data.x + data.w] = ASCII_IRC;
+void createBase(char map[HEIGHT][WIDTH][DEPTH], bd data) {
+  char color[9];
+  if (data.id == 51) {
+    strcpy (color, "\033[0;31m");
+  }else{
+    strcpy (color, "\033[0;34m");
+  }
+
+  //strcat(color, "\201");
+  strcpy(map[data.y - 1][data.x - 1], color);
+  map[data.y][data.x][0] = ASCII_SLC;
+  map[data.y + data.h ][data.x][0] = ASCII_ILC; //CHECK SIZE
+  map[data.y][data.x + data.w][0] = ASCII_SRC;
+  map[data.y + data.h ][data.x + data.w][0] = ASCII_IRC;
 
   for (int i = 1; i < data.h; i++) { //CHECK SIZE
-    map[data.y + i][data.x] = ASCII_VER;
-    map[data.y + i][data.x + data.w] = ASCII_VER; //CHECK SIZE
+    map[data.y + i][data.x][0] = ASCII_VER;
+
+    map[data.y + i][data.x + data.w][0] = ASCII_VER; //CHECK SIZE
   }
 
   for (int i = 1; i < data.w; i++) { //CHECK SIZE
-    map[data.y][data.x + i] = ASCII_HOR;
-    map[data.y + data.h][data.x + i] = ASCII_HOR; //CHECK SIZE
+    map[data.y][data.x + i][0] = ASCII_HOR;
+    map[data.y + data.h][data.x + i][0] = ASCII_HOR; //CHECK SIZE
   }
-  //nameBase(0, 24, name, map);
+  nameBase(data, map);
+
 }
 
 
-void nameBase(int locationY, int locationX, char name, char map[HEIGHT][WIDTH]){
+void nameBase(bd data, char map[HEIGHT][WIDTH][DEPTH]){
   for (int i = 1; i < 8; i++) {
     switch (i) {
       case 1:
-        map[locationY + 1][locationX + i] = 66;
+        map[data.y + 1][data.x + i][0] = ASCII_BMAYUS;
         break;
       case 2:
-        map[locationY + 1][locationX + i] = 97;
+        map[data.y + 1][data.x + i][0] = ASCII_A;
         break;
       case 3:
-        map[locationY + 1][locationX + i] = 115;
+        map[data.y + 1][data.x + i][0] = ASCII_S;
         break;
       case 4:
-        map[locationY + 1][locationX + i] = 101;
+        map[data.y + 1][data.x + i][0] = ASCII_E;
         break;
       case 5:
-        map[locationY + 1][locationX + i] = 0;
+        map[data.y + 1][data.x + i][0] = 42;
         break;
       case 6:
-        map[locationY + 1][locationX + i] = 35;
+        map[data.y + 1][data.x + i][0] = ASCII_NUM;
         break;
       case 7:
-        map[locationY + 1][locationX + i] = name;
+        map[data.y + 1][data.x + i][0] = data.id;
         break;
     }
   }
